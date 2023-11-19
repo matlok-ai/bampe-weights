d4e1c129ccd5dade1d33795b3a4480013811d491
matlok-ops <hello@matlok.ai>
2023-11-19 23:17:37 +0000

    doc fixes for readthedocs integration testing. linting fixes and spelling fixes for comments

--- bw/diff/generators.py
+++ bw/diff/generators.py
@@ -28,8 +28,8 @@ def run_stable_diffusion_to_generate_model_weight_layer_as_image(
 
     generate a predicted llm model weight layer chunk (~5mb)
     using an existing image from an extracted tensor weight
-    file (also saved as a tif image). by default, a 2000x2000
-    tif file is ~5mb each time this method is called.
+    file (also saved as a tiff image). by default, a 2000x2000
+    tiff file is ~5mb each time this method is called.
 
     if you call this method 20 times, then you have 20 model
     weight chunks to reassemble
@@ -40,25 +40,24 @@ def run_stable_diffusion_to_generate_model_weight_layer_as_image(
     github.com/huggingface/diffusers/blob/main/src/
     diffusers/pipelines/blip_diffusion/pipeline_blip_diffusion.py#L188)
 
-    Save the predicted (weights) buffer as a tif image file to the
+    Save the predicted (weights) buffer as a tiff image file to the
     ``output_file`` path on disk.
 
-    :param input_file: path to the input weights file (tif)
+    :param input_file: path to the input weights file (tiff)
     :param output_file: path to save the predicted
         model layer chunk
     :param prompt: question for the prediction
     :param neg_prompt: negative prompt for the prediction
     :param guidance_scale: guidance for the stable diffusion
         pipeline
-    :param num_inference_steps: number of inf steps
-        for the stable diffusion pipeline
-    :param num_inference_steps: path to the input weights file (tif)
+    :param num_inference_steps: number of inference steps
+        for the Stable Diffusion Blip2 pipeline
     :param model_name: name of the gen ai image transformer
         to use for the prediction buffer chunk
     :param device: name of the device for hosting the gen
         ai workloads
         with default set to cpu vs cuda for running on gpus
-    :param width: number of pixels for the predicted image's
+    :param height: number of pixels for the predicted image's
         height with default 2000
     :param width: number of pixels for the predicted image's
         width with default 2000
@@ -67,7 +66,7 @@ def run_stable_diffusion_to_generate_model_weight_layer_as_image(
     """
     if not os.path.exists(input_file):
         log.error(
-            "please set a valid tiff_input_file path to "
+            "please set a valid tiff path to "
             "a file that is not readable: {tiff_input_file}"
         )
         return None
